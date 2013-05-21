407d38cb2fe3b61245ec2a2cdbdf52582f698b57
Adel Gadllah <adel.gadllah@gmail.com>
2013-05-21 23:01:23 +0200

    Communicator: Make factory instances static

--- src/monitor/Communicator.java
+++ src/monitor/Communicator.java
@@ -14,9 +14,12 @@ import utils.ServiceException;
 
 public class Communicator {
 
+	private static SOAPConnectionFactory connectionFactory;
+	private static MessageFactory msgFactory;
+
 	/**
-	 * Sends a request to the webservice specified
-	 * and returns the response as XML string
+	 * Sends a request to the webservice specified and returns the response as
+	 * XML string
 	 * 
 	 * @param serviceUrl
 	 * @param xmlRequest
@@ -24,26 +27,32 @@ public class Communicator {
 	 * @throws ServiceException
 	 */
 	public String sendRequest(String serviceUrl, String xmlRequest) throws ServiceException {
-		
-		ByteArrayOutputStream result = new ByteArrayOutputStream();
-		
-		try {
-			SOAPConnectionFactory sfc = SOAPConnectionFactory.newInstance();
-			MessageFactory msgFactory = MessageFactory.newInstance();
-	
-		    SOAPConnection connection = sfc.createConnection();
-			
-		    MimeHeaders mimeHeaders = new MimeHeaders();
-	        mimeHeaders.addHeader("Content-Type","text/xml; charset=UTF-8");
 
-	        // TODO: hardcoded
-	        xmlRequest = "<soapenv:Envelope xmlns:soapenv='http://schemas.xmlsoap.org/soap/envelope/' xmlns:ser='http://service/'>" + "<soapenv:Header/>" + "<soapenv:Body>"
-					+ "<ser:doCalc>" + "<a>18</a>" + "<b>50</b>" + "</ser:doCalc>" + "</soapenv:Body>" + "</soapenv:Envelope>";
-	        
-	        // TODO: hardcoded
-	        URL endpoint = new URL("http://localhost:9999/WS/Test");
-	        SOAPMessage response = connection.call(msgFactory.createMessage(mimeHeaders, new ByteArrayInputStream(xmlRequest.getBytes())), endpoint);
-	        
+		ByteArrayOutputStream result = new ByteArrayOutputStream();
+
+		try {
+			if (Communicator.connectionFactory == null) {
+				Communicator.connectionFactory = SOAPConnectionFactory.newInstance();
+			}
+
+			if (Communicator.msgFactory == null) {
+				Communicator.msgFactory = MessageFactory.newInstance();
+			}
+
+			SOAPConnection connection = Communicator.connectionFactory.createConnection();
+
+			MimeHeaders mimeHeaders = new MimeHeaders();
+			mimeHeaders.addHeader("Content-Type", "text/xml; charset=UTF-8");
+
+			// TODO: hardcoded
+			xmlRequest = "<soapenv:Envelope xmlns:soapenv='http://schemas.xmlsoap.org/soap/envelope/' xmlns:ser='http://service/'>" + "<soapenv:Header/>" + "<soapenv:Body>" + "<ser:doCalc>"
+					+ "<a>18</a>" + "<b>50</b>" + "</ser:doCalc>" + "</soapenv:Body>" + "</soapenv:Envelope>";
+
+			// TODO: hardcoded
+			URL endpoint = new URL("http://localhost:9999/WS/Test");
+			SOAPMessage request = Communicator.msgFactory.createMessage(mimeHeaders, new ByteArrayInputStream(xmlRequest.getBytes()));
+			SOAPMessage response = connection.call(request, endpoint);
+
 			response.writeTo(result);
 		} catch (Exception e) {
 			throw new ServiceException(e);
