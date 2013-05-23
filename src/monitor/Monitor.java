d1d8898e98268efeaf6bf30390dd9753d04e4738
Adel Gadllah <adel.gadllah@gmail.com>
2013-05-23 18:18:21 +0200

    monitor: Log changes to database

--- src/monitor/Monitor.java
+++ src/monitor/Monitor.java
@@ -1,11 +1,13 @@
 package monitor;
 
+import java.util.Date;
 import java.util.List;
 
 import org.apache.log4j.Logger;
 
 import database.dao.ResourceFactory;
 import database.entity.Data;
+import database.entity.Log;
 import database.entity.WebService;
 
 import utils.ServiceException;
@@ -23,6 +25,16 @@ public class Monitor implements Runnable {
 		this.service = service;
 	}
 
+	private void logChange(WebService webService, String method, Log.Type type, String message) {
+		Log entry = new Log();
+		entry.setMessage(message);
+		entry.setName(method);
+		entry.setType(type);
+		entry.setTimestamp(new Date());
+		entry.setWebservice(webService);
+		ResourceFactory.getLogDao().addLog(entry);
+	}
+	
 	@Override
 	public void run() {
 		log.info("Starting monitoring of " + service);
@@ -36,17 +48,20 @@ public class Monitor implements Runnable {
 				// Verify method responses
 				for (Data data : dataList) {
 					if (!Soap.isAvailable(service)) {
+						logChange(webService, "", Log.Type.AVAILABILITY, "Server not reachable");
 						throw new ServiceException("Server not reachable");
 					}
 					String msg = Soap.sendRequest(service, data.getMethod(), data.getRequest());
 					if (!msg.equals(data.getResponse())) {
 						// TODO: Log to DB
 						log.warn("Message response changed for method " + data.getMethod() + "!");
+						logChange(webService, data.getMethod(), Log.Type.OPERATION, "Response changed!");
 					}
 				}
 				log.debug("Waiting between checks");
 			} catch (ServiceException e) {
 				// TODO: Log to DB
+				logChange(webService, "", Log.Type.AVAILABILITY, "Server not reachable");
 				log.warn(e.getMessage());
 			}
 			catch (InterruptedException e) {
